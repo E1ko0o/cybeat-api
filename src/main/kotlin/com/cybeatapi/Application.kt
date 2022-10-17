54d48951d506add3193815df290311e196efc9e2
e1ko0o <slava121231@mail.ru>
2022-10-17 18:10:15 +0300

    prepare app for deploying

--- src/main/kotlin/com/cybeatapi/Application.kt
+++ src/main/kotlin/com/cybeatapi/Application.kt
@@ -3,9 +3,10 @@ package com.cybeatapi
 import com.cybeatapi.dao.DatabaseFactory
 import io.ktor.server.application.*
 import com.cybeatapi.plugins.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit =
-        io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
